a315af3678bc3af8b472e1ea972887e4d723a7d6
bobur554396 <bobur.muhsimbaev@gmail.com>
2016-08-06 21:50:31 +0600

    asd

--- my_folder/test.py
+++ my_folder/test.py
@@ -12,7 +12,7 @@ colors = np.random.rand(N)
 area = np.pi * (45 * np.random.rand(N))**2
 
 
-plt.scatter(x, y, s=area, c=colors, alpha=0.4, label="Test label")
+plt.scatter	(x, y, s=area, c=colors, alpha=0.4, label="Test label")
 plt.legend()
 plt.xlabel("X")
 plt.ylabel("Y")
